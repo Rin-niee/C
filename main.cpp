9b534ef720a89d6d3b6b4f80ed9d02e4dc12850d
Беседина Елизавета <125102825+BesedinaElizaveta@users.noreply.github.com>
2023-02-17 14:06:09 +1000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{
+	douple x1,x2,d,a,b,c;
+	a =15, b=16,c=1
+	d = b*b-4*a*c;
+	if (d>0)
+	{
+		x1=(((-b)+sqrt(d))/(2*a));
+		x1=(((-b)+sqrt(d))/(2*a));
+	}
+	if (d==0)
+	{
+		x1=-(b/(2*a));
+	}
+	if (d<0)
+	{
+		
+	}
+	return 0;
+}
